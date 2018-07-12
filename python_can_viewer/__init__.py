d093a8c28cbbee9cb0e9181a9c7c2639073bb0e3
Kristian Sloth Lauszus <lauszus@gmail.com>
2018-07-12 17:45:35 +0200

    Added missing headers and setup.py entires

--- python_can_viewer/__init__.py
+++ python_can_viewer/__init__.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2018 Kristian Sloth Lauszus. All rights reserved.
+#
+# Contact information
+# -------------------
+# Kristian Sloth Lauszus
+# Web      :  http://www.lauszus.com
+# e-mail   :  lauszus@gmail.com
+
+__version__ = '0.1.0'
+
 from .python_can_viewer import *
--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2018 Kristian Sloth Lauszus. All rights reserved.
+#
+# Contact information
+# -------------------
+# Kristian Sloth Lauszus
+# Web      :  http://www.lauszus.com
+# e-mail   :  lauszus@gmail.com
+
+import re
 from setuptools import setup, find_packages
+
+with open('python_can_viewer/__init__.py', 'r') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        f.read(), re.MULTILINE).group(1)
 
 with open('README.md', 'r') as f:
     long_description = f.read()
@@ -18,14 +33,21 @@ setup(
     url='https://github.com/Lauszus/python_can_viewer',
     description='A simple CAN viewer terminal application written in Python',
     long_description=long_description,
-    version='0.0.1',
+    version=version,
     packages=find_packages(),
     author='Kristian Sloth Lauszus',
     author_email='lauszus@gmail.com',
-    license="GPLv2",
+    license='GPLv2',
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=2.7',
     install_requires=['python-can', 'six', 'typing'],
     extras_require=extras_require,
     tests_require=tests_require,
+    classifiers=(
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+    ),
 )
